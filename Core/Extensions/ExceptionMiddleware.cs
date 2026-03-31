0dbe958022d1d7c516c2f8db364b3868f1323e03
Alptekin <alptekin.ocakdan@gmail.com>
2026-03-31 04:05:06 +0300

    Key and expection handling changed

--- Core/Extensions/ExceptionMiddleware.cs
+++ Core/Extensions/ExceptionMiddleware.cs
@@ -25,7 +25,7 @@ namespace Core.Extensions {
             httpContext.Response.ContentType = "application/json";
             httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-            string message = "Internal Server Error";
+            string message = e.Message;
             IEnumerable<ValidationFailure> errors;
             if (e.GetType() == typeof(ValidationException)) {
                 message = "Validation Error";
